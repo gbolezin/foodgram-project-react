9dd4527af5ae86887a640359f534b65295723ef3
Георгий Болезин <g.bolezin@zenith24.ru>
2024-01-19 12:11:16 +0700

    Add CSRF Token-1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '[*]').split(',')
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '[*]').split(',')
+CSRF_TRUSTED_ORIGINS = ['https://foodgram.zenith24.ru']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
