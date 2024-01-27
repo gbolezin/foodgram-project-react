75118cf527d6367e1a37261ca24a07ead7788487
Георгий Болезин <g.bolezin@zenith24.ru>
2024-01-27 12:21:20 +0700

    After third review

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-
 import recipes.constants as constants
 
 
